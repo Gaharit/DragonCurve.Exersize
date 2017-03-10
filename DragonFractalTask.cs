adae27f8b46a437126c385044185d72f9ffda4e7
Gaharit <Gaharit@gmail.com>
2017-03-10 23:59:29 +0800

    Add project files.

---
+++ DragonFractalTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{
+		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+            double x = 1, y = 0, ang, dx;
+            var random = new Random(seed);
+            pixels.SetPixel(x, y);
+            while (iterationsCount-- > 0)
+            {
+                var rand = random.Next(2);
+                    ang = Math.PI / 4 + Math.PI / 2 * rand;
+                    dx = (x * Math.Cos(ang) - y * Math.Sin(ang))
+                        / Math.Sqrt(2.0) + rand;
+                    y = (x * Math.Sin(ang) + y * Math.Cos(ang))
+                        / Math.Sqrt(2.0);
+                x = dx;
+                pixels.SetPixel(x, y);
+            }
+		}
+	}
+}
